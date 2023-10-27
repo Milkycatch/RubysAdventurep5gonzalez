d6bbdfc198d54d80d77be8d0b1430660d4e075ae
Joel Gonzalez <754537@student.dusd.net>
2023-10-26 18:02:32 -0700

    ruby ya se mueve
    ya se mueve ruby

--- RubysAdventurep5/Assets/Scripts/RubyController.cs
+++ RubysAdventurep5/Assets/Scripts/RubyController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,21 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal; 
+        position.y = position.y + 0.1f * vertical;
+
+        transform.position = position;
     }
 }
