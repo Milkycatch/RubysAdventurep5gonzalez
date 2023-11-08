783aa77c8968909cdfca9ccc74a250e3faf63496
Joel Gonzalez <754537@student.dusd.net>
2023-11-08 13:50:10 -0800

    el robot se mueve  para los lados  con animaciiones

--- RubysAdventurep5/Assets/Scripts/EnemyController.cs
+++ RubysAdventurep5/Assets/Scripts/EnemyController.cs
@@ -1,28 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
     
     Rigidbody2D rigidbody2;
+
+    float timer;
+    int direction = 1;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2 = GetComponent<Rigidbody2D>();
+        timer = changeTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
     }
-    private void FixedUpdate()
+     void FixedUpdate()
     {
         Vector2   position = rigidbody2.position;
-        position.x = position.x + Time.deltaTime;
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed *direction;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime *speed  *  direction;
+        }
+       
 
         rigidbody2.MovePosition(position);
     }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+        if (player != null) 
+        {
+            player.ChangeHealth(-1);
+        }
+       
+    }
+
 }
+
